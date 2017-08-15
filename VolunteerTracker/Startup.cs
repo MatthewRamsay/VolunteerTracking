33edd44275cedbbb5fbd34f6b6146520a5b657dc
MatthewRamsay <Matthew.t.ramsay@gmail.com>
2017-08-15 19:01:56 -0400

    Add project files.

---
+++ VolunteerTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VolunteerTracker.Startup))]
+namespace VolunteerTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
