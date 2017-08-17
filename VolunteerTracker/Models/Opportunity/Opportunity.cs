9ad5a4767ed17af0aaaa89834fae0174c0ccbe05
Katie Grubbs <katie.grubbs@gmail.com>
2017-08-17 14:08:26 -0400

    Added Opportunity Model

---
+++ VolunteerTracker/Models/Opportunity/Opportunity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VolunteerTracker.Models.Volunteer;
+
+namespace VolunteerTracker.Models.Opportunity
+{
+    public class Opportunity
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public Center Location { get; set; }
+    }
+}
