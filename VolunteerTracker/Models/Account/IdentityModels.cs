165aab4d2a63d94536d3273a165781b78c9372cf
Katie Grubbs <katie.grubbs@gmail.com>
2017-08-18 15:21:01 -0400

    Delete This

--- VolunteerTracker/Models/Account/IdentityModels.cs
+++ VolunteerTracker/Models/Account/IdentityModels.cs
@@ -37,5 +37,7 @@ namespace VolunteerTracker.Models
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<Opportunity.Opportunity> Opportunities { get; set; }
+
+        public System.Data.Entity.DbSet<VolunteerTracker.ViewModels.VolunteerViewModel> VolunteerViewModels { get; set; }
     }
 }
