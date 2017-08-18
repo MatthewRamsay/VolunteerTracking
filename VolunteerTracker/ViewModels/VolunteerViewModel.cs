6c7db3368645552d31edbfc65ff61c840d3e9f98
Katie Grubbs <katie.grubbs@gmail.com>
2017-08-17 21:20:11 -0400

    Updated NuGet Package Manager & Added Volunteer ViewModel

---
+++ VolunteerTracker/ViewModels/VolunteerViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VolunteerTracker.Models.Volunteer;
+
+namespace VolunteerTracker.ViewModels
+{
+    public class VolunteerViewModel
+    {
+        [Display(Name = "First Name")]
+        [StringLength(35)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [StringLength(35)]
+        public string LastName { get; set; }
+
+        [Display(Name = "Preferred Center(s)")]
+        public IList<Center> CentersPreferred { get; set; }
+
+        [Display(Name = "Availability")]
+        public IList<DayAvailable> DaysAvailable { get; set; }
+
+        [Display(Name = "Education")]
+        public string HighestEducation { get; set; }
+
+        [Display(Name = "Skills")]
+        public IList<Skill> Skills { get; set; }
+
+        [Display(Name = "Licenses")]
+        public IList<CurrentLicense> Licenses { get; set; }
+
+        // Status of volunteer (Approved, Pending, Disapproved, Inactive)
+        [Display(Name = "Status")]
+        public IList<Status> Statuses { get; set; }
+    }
+}
